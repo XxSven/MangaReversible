c2f0535da2be3a8c82332a3ecfb8d2508367b2be
Theo Soule <tsoule@cegid.com>
2023-06-27 16:25:37 +0200

    [UPD] DATA : Ajout du JSON pour ajouter les mangas

--- index.js
+++ index.js
@@ -1,53 +1,90 @@
-'use strict';
-window.addEventListener('load', init);
+function rotateHeader () {
+  const headerNode = document.getElementsByClassName('rotating-header')[0];
+  if (headerNode.classList.contains('rotate')) {
+    headerNode.classList.remove('rotate');
+  } else {
+    headerNode.classList.add('rotate');
+  }
+}
 
-function init() {
-    var homeButton = document.getElementsByClassName('home-button')[0],
-        listButton = document.getElementsByClassName('list-button')[0],
-        contactButton = document.getElementsByClassName('contact-button')[0];
+function refreshMenu (currentButton) {
+  [...document.getElementsByClassName('menu__button')].forEach((button) => {
+    button.classList.remove('menu__button--current');
+  });
+  currentButton.classList.add('menu__button--current');
+}
 
-    homeButton.addEventListener('click', displayContent.bind(null, 'home', homeButton));
-    listButton.addEventListener('click', displayContent.bind(null, 'list', listButton));
-    contactButton.addEventListener('click', displayContent.bind(null, 'contact', contactButton));
+function refreshMainContent(contentName) {
+  var template = document.getElementById(contentName).content;
+  document.getElementsByClassName('main-content')[0].innerHTML = '';
+  document.getElementsByClassName('main-content')[0].appendChild(template.cloneNode(true));
+}
 
-    [...document.getElementsByClassName('flip-banner-button')].forEach((button)=>{
-        button.addEventListener('click', rotateHeader);
+function showDetail(manga) {
+  var mangaContentNode = document.getElementsByClassName('manga-content')[0],
+    imageNode;
+  mangaContentNode.innerHTML = '';
+  if(manga.covers){
+    manga.covers.forEach(cover=>{
+      if(cover.imageVariant){
+        imageNode = document.createElement('img');
+        imageNode.setAttribute('src','./images/'+cover.imageVariant);
+        mangaContentNode.appendChild(imageNode);
+      }
     });
-
-    displayContent('home', homeButton);
+  }
 }
 
-function displayContent(pageName, currentButton) {
-    var template;
-    switch(pageName) {
-        case 'list':
-            template = document.getElementById('list-content').content;
-            break;
-        case 'contact':
-            template = document.getElementById('contact-content').content;
-            break;        
-        case 'home':
-        default:
-            template = document.getElementById('home-content').content;
-    }
-    document.getElementsByClassName('content')[0].innerHTML = '';
-    document.getElementsByClassName('content')[0].appendChild(template.cloneNode(true));
-
-    refreshMenu(currentButton);
+function displayMangaContent() {
+  var mangaList = document.getElementsByClassName('manga-list')[0];
+  fetch(`${location.pathname.replace(/\/[^/]*\..*/, '')
+    .replace(/\/$/, '')}/data.json`)
+    .then((response) => response.json())
+    .then((json) => {
+      var mangas = json.mangas,
+        fragment = document.createDocumentFragment();
+      mangas.forEach(manga=>{
+        let liNode = document.createElement('li');
+        liNode.addEventListener('click',showDetail.bind(null, manga));
+        liNode.innerText = manga?.name;
+        fragment.appendChild(liNode);
+        manga?.name;
+      });
+      mangaList.appendChild(fragment);
+    });
 }
 
-function rotateHeader() {
-    var headerNode = document.getElementsByClassName('rotating-header')[0];
-    if(headerNode.classList.contains('rotate')){
-        headerNode.classList.remove('rotate');
-    } else {
-        headerNode.classList.add('rotate');
-    }
+function displayContent (pageName, currentButton) {
+  switch (pageName) {
+  case 'list':
+    refreshMainContent('list-content');
+    displayMangaContent();
+    break;
+  case 'contact':
+    refreshMainContent('contact-content');
+    break;
+  case 'home':
+  default:
+    refreshMainContent('home-content');
+  }
+
+  refreshMenu(currentButton);
 }
 
-function refreshMenu(currentButton) {
-    [...document.getElementsByClassName('menu__button')].forEach((button)=>{
-        button.classList.remove('menu__button--current');
-    })
-    currentButton.classList.add('menu__button--current');
+function init () {
+  const homeButton = document.getElementsByClassName('home-button')[0];
+  const listButton = document.getElementsByClassName('list-button')[0];
+  const contactButton = document.getElementsByClassName('contact-button')[0];
+
+  homeButton.addEventListener('click', displayContent.bind(null, 'home', homeButton));
+  listButton.addEventListener('click', displayContent.bind(null, 'list', listButton));
+  contactButton.addEventListener('click', displayContent.bind(null, 'contact', contactButton));
+
+  [...document.getElementsByClassName('flip-banner-button')].forEach((button) => {
+    button.addEventListener('click', rotateHeader);
+  });
+
+  displayContent('home', homeButton);
 }
+
+window.addEventListener('load', init);
